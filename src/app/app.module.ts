9896d173e381d02d53f470c837641f07de7073e0
Karine <martinskazk@gmail.com>
2022-07-18 18:27:24 -0300

    Filtro de arquivos necessários + roteamento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
+import { PagesModule } from './pages/pages.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
+    PagesModule,
     BrowserModule,
     AppRoutingModule
   ],
