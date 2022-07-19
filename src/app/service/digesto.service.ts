07164ae0d855f770e237466a010a473c017b5792
Karine <martinskazk@gmail.com>
2022-07-18 21:51:47 -0300

    filtro de processos pelo cnj

--- src/app/service/digesto.service.ts
+++ src/app/service/digesto.service.ts
@@ -1,9 +1,25 @@
+import { environment } from './../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DigestoService {
   constructor(private http: HttpClient) {}
+
+  public getProcessoByCNJ(cnj: string): Observable<any> {
+    const url = `${environment.API}/${cnj}${environment.tipoNumero}`;
+    console.log(url);
+
+    return this.http
+      .get<any>(url, {
+        headers: {
+          Authorization:
+            "Basic NWFmOGJhNGMtNDNlMy00MzYxLTllOWMtZjczNDU4YWI2YTViOg==",
+          },
+      })
+      .pipe(map((res) => res));
+  }
 }
