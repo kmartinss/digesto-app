d6893f997900ebb37b24c5196d1eb5a4f2499e52
Karine <martinskazk@gmail.com>
2022-07-18 21:51:26 -0300

    Criação de variáveis de ambiente

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,6 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  API: 'https://op.digesto.com.br/api/tribproc',
+  apiToken: '5af8ba4c-43e3-4361-9e9c-f73458ab6a5b',
+  tipoNumero: '?tipo_numero=8'
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
