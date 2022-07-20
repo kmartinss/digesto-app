2a5356183a355b625b78725f32ebf703c12e81b3
Karine <martinskazk@gmail.com>
2022-07-20 12:03:25 -0300

    Preenchimento de environment prod pra deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  API: 'https://op.digesto.com.br/api/tribproc',
+  apiToken: '3013c791-18ea-48ff-b4ef-ad5cae624cb6',
+  tipoNumero: '?tipo_numero=8'
 };
